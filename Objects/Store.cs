8e19f868baf89fdc2b04e1fb6c3eb98df2c31869
Michael Dada <mail.michaeldada@gmail.com>
2016-03-04 12:53:12 -0800

    add test to check if stores is empty

--- Objects/Store.cs
+++ Objects/Store.cs
@@ -15,5 +15,190 @@ namespace Shoes
       _name = Name;
     }
 
+    public override bool Equals(System.Object otherStore)
+    {
+      if (!(otherStore is Store))
+      {
+        return false;
+      }
+      else
+      {
+        Store newStore = (Store) otherStore;
+        bool idEquality = this.GetId() == newStore.GetId();
+        bool nameEquality = this.GetName() == newStore.GetName();
+        return (idEquality && nameEquality);
+      }
+    }
+
+    public int GetId()
+    {
+      return _id;
+    }
+    public string GetName()
+    {
+      return _name;
+    }
+    public void SetName(string newName)
+    {
+      _name = newName;
+    }
+    public static List<Store> GetAll()
+    {
+      List<Store> allStores = new List<Store>{};
+
+      SqlConnection conn = DB.Connection();
+      SqlDataReader rdr = null;
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("SELECT * FROM stores;", conn);
+      rdr = cmd.ExecuteReader();
+
+      while(rdr.Read())
+      {
+        int storeId = rdr.GetInt32(0);
+        string storeName = rdr.GetString(1);
+        Store newStore = new Store(storeName, storeId);
+        allStores.Add(newStore);
+      }
+
+      if(rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        conn.Close();
+      }
+
+      return allStores;
+    }
+
+    public void Save()
+    {
+      SqlConnection conn = DB.Connection();
+      SqlDataReader rdr;
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("INSERT INTO stores (name) OUTPUT INSERTED.id VALUES (@StoreName);", conn);
+
+      SqlParameter nameParameter = new SqlParameter();
+      nameParameter.ParameterName = "@StoreName";
+      nameParameter.Value = this.GetName();
+      cmd.Parameters.Add(nameParameter);
+      rdr = cmd.ExecuteReader();
+
+      while(rdr.Read())
+      {
+        this._id = rdr.GetInt32(0);
+      }
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if(conn != null)
+      {
+        conn.Close();
+      }
+    }
+
+    public void Update(string newName)
+    {
+      SqlConnection conn = DB.Connection();
+      SqlDataReader rdr;
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("UPDATE stores SET name = @NewName OUTPUT INSERTED.name WHERE id = @StoreId;", conn);
+
+      SqlParameter newNameParameter = new SqlParameter();
+      newNameParameter.ParameterName = "@NewName";
+      newNameParameter.Value = newName;
+      cmd.Parameters.Add(newNameParameter);
+
+
+      SqlParameter storeIdParameter = new SqlParameter();
+      storeIdParameter.ParameterName = "@StoreId";
+      storeIdParameter.Value = this.GetId();
+      cmd.Parameters.Add(storeIdParameter);
+      rdr = cmd.ExecuteReader();
+
+      while(rdr.Read())
+      {
+        this._name = rdr.GetString(0);
+      }
+
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+
+      if (conn != null)
+      {
+        conn.Close();
+      }
+    }
+
+    public static void DeleteAll()
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+      SqlCommand cmd = new SqlCommand("DELETE FROM stores;", conn);
+      cmd.ExecuteNonQuery();
+    }
+
+    public void Delete()
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("DELETE FROM stores WHERE id = @StoreId;", conn);
+
+      SqlParameter storeIdParameter = new SqlParameter();
+      storeIdParameter.ParameterName = "@StoreId";
+      storeIdParameter.Value = this.GetId();
+
+      cmd.Parameters.Add(storeIdParameter);
+      cmd.ExecuteNonQuery();
+
+      if (conn != null)
+      {
+        conn.Close();
+      }
+    }
+
+    public static Store Find(int id)
+    {
+      SqlConnection conn = DB.Connection();
+      SqlDataReader rdr = null;
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("SELECT * FROM stores WHERE id = @StoreId;", conn);
+      SqlParameter storeIdParameter = new SqlParameter();
+      storeIdParameter.ParameterName = "@StoreId";
+      storeIdParameter.Value = id.ToString();
+      cmd.Parameters.Add(storeIdParameter);
+      rdr = cmd.ExecuteReader();
+
+      int foundStoreId = 0;
+      string foundStoreName = null;
+
+      while(rdr.Read())
+      {
+        foundStoreId = rdr.GetInt32(0);
+        foundStoreName = rdr.GetString(1);
+      }
+      Store foundStore = new Store(foundStoreName, foundStoreId);
+
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        conn.Close();
+      }
+      return foundStore;
+    }
+
+
   }
 }
---
+++ Tests/StoreTest.cs
@@ -0,0 +1,29 @@
+using Xunit;
+using System.Collections.Generic;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Shoes
+{
+  public class StoreTest : IDisposable
+  {
+    public StoreTest()
+    {
+      DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=shoe_stores_test;Integrated Security=SSPI;";
+    }
+
+    public void Dispose()
+    {
+       Store.DeleteAll();
+    }
+    
+    [Fact]
+    public void Test_StoresEmptyAtFirst()
+    {
+      int result = Store.GetAll().Count;
+
+      Assert.Equal(0, result);
+    }
+  }
+}
