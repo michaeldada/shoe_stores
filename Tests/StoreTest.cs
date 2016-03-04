2f1c77f87161f94f8da401305eecb42f486e57a1
Michael Dada <mail.michaeldada@gmail.com>
2016-03-04 12:58:42 -0800

    add update and delete functionality for stores

--- Tests/StoreTest.cs
+++ Tests/StoreTest.cs
@@ -34,5 +34,67 @@ namespace Shoes
 
       Assert.Equal(firstStore, secondStore);
     }
+    [Fact]
+    public void Test_Save_SavesStoreToDatabase()
+    {
+      //Arrange
+      Store testStore = new Store("Sears");
+      testStore.Save();
+
+      //Act
+      List<Store> result = Store.GetAll();
+      List<Store> testList = new List<Store>{testStore};
+
+      //Assert
+      Assert.Equal(testList, result);
+    }
+
+    [Fact]
+    public void Test_Save_AssignsIdToStoreObject()
+    {
+      //Arrange
+      Store testStore = new Store("Sears");
+      testStore.Save();
+
+      //Act
+      Store savedStore = Store.GetAll()[0];
+
+      int result = savedStore.GetId();
+      int testId = testStore.GetId();
+
+      //Assert
+      Assert.Equal(testId, result);
+    }
+
+    [Fact]
+    public void Test_Find_FindsStoreInDatabase()
+    {
+      //Arrange
+      Store testStore = new Store("Sears");
+      testStore.Save();
+
+      //Act
+      Store foundStore = Store.Find(testStore.GetId());
+
+      //Assert
+      Assert.Equal(testStore, foundStore);
+    }
+
+    [Fact]
+    public void Test_GetStores_RetrievesAllStores()
+    {
+      Store firstStore = new Store("Sears");
+      firstStore.Save();
+      Store secondStore = new Store("Target");
+      secondStore.Save();
+
+      List<Store> testStoreList = new List<Store> {firstStore, secondStore};
+      List<Store> resultStoreList = Store.GetAll();
+
+      Assert.Equal(testStoreList, resultStoreList);
+
+
+
+    }
   }
 }
